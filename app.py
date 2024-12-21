6950ad668c3330cf2a84473bb2d430c098574ef1
Asutosh Pandya <ashu936@gmail.com>
2024-12-20 18:51:59 -0600

    Add Logging with Client-CorrelationId

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import logging
+import uuid
 
 # Create a Flask application
 app = Flask(__name__)
@@ -14,23 +15,30 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 
+@app.before_request
+def add_correlation_id():
+    request.correlation_id = str(uuid.uuid4())
+    print(f"Correlation ID: {request.correlation_id}")
+
+
 # Root endpoint
 @app.route('/')
 def home():
-    logger.info('Handling root endpoint')
+    logger.info('Handling root endpoint %s', request.correlation_id)
     return jsonify({"message": "Welcome to the Car API!"})
 
 
 # Get all cars
 @app.route('/cars', methods=['GET'])
 def get_cars():
-    logger.info('Handling Get All Cars endpoints %s', jsonify(cars))
+    logger.info('Handling Get All Cars endpoints %s %s', jsonify(cars), request.correlation_id)
     return jsonify(cars)
 
 
 # Get a specific car by ID
 @app.route('/cars/<int:car_id>', methods=['GET'])
 def get_car(car_id):
+    logger.info('Handling Car by carId %s', request.correlation_id)
     car = next((car for car in cars if car["id"] == car_id), None)
     if car:
         return jsonify(car)
